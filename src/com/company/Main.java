d3b4b55e611e70c983adee8cae66d5ac0a1ed712
funtushan@gmail.com <funtushan@gmail.com>
2020-09-22 20:48:20 +0300

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import com.company.test.TestRun;
+
+public class Main {
+
+    public static void main(String[] args) {
+        TestRun testRun = new TestRun();
+        testRun.print();
+        testRun.runMyNewApplication();
+    }
+}
---
+++ src/com/company/test/TestRun.java
@@ -0,0 +1,14 @@
+package com.company.test;
+
+public class TestRun {
+
+    private int anInt = 10;
+
+    public void print() {
+        System.out.println("hello world: " + anInt);
+    }
+
+    public void runMyNewApplication() {
+        System.out.println("TestRun.runMyNewApplication");
+    }
+}
